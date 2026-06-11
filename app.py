4eaff56c47a6a1b6b41f3d7a86b1168757dda4e1
TaKo86 <warneranash@gmail.com>
2026-06-11 16:11:16 +1200

    Reapply "refactor app into blueprint structure and fixed model attribute refre…"
    This reverts commit 0c37793f2a204618750973b759d583bed46b34d3.

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from models import db
+from blueprints.auth import auth_bp
+from blueprints.expenses import expenses_bp
+from blueprints.income import income_bp
+from blueprints.bills import bills_bp
+from utils import login_required
+import os
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = "warnalia"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tracking_app.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(expenses_bp)
+    app.register_blueprint(income_bp)
+    app.register_blueprint(bills_bp)
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        db.create_all()#create database tables if they don't exist
+    app.run(debug=True)
